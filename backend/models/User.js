7f2aa6064a57417a65a2b1c66e14a5a598f1dd94
Shruti Potdar <162474921+potdarshruti@users.noreply.github.com>
2026-07-16 15:16:31 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,19 +1,10 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-});
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  ip: { type: String },
+}, { timestamps: true });
 
 export default mongoose.model("User", userSchema);
